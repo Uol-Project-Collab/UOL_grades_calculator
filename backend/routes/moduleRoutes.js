7e56462ce49a35407db83decf2cfb96ba16dd468
Akshit Sankhala <akshitsankhala14@gmail.com>
2025-04-08 00:21:50 +0530

    UGC 55 endpoint to retrieve all modules completed, do crosscheck minor changes in firestore.rules and firestore.index.json

--- backend/routes/moduleRoutes.js
+++ backend/routes/moduleRoutes.js
@@ -1,34 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { 
-  getModules, 
-  createModule, 
-  updateModule, 
-  deleteModule 
-} = require('../controllers/moduleController');
+const { getModules } = require('../controllers/moduleController');
 
-// GET all modules + POST new module
-router.route('/modules')
-  .get(getModules)
-  .post(createModule);
-
-// PUT update + DELETE specific module
-router.route('/modules/:id')
-  .put(updateModule)
-  .delete(deleteModule);
+// Get all available modules
+router.get('/modules', getModules);
 
 module.exports = router;
-
-
-// !!! Endpoint for this task: !!!
-
-// To show the list of modules in the form
-// GET/.../modules -> get ALL modules as json object
-
-
-// For the next tasks:
-
-// To assign modules to a particular user
-// POST/.../users/add-module/:{userId, [{1, ITP1, 4, T, 100}]} -> add to db
-// To send modules from that particular user (as json object)
-// GET/.../users/get-modules/:userId
