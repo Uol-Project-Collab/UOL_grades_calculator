4f1ea5b8a7fa66b993e280210b36dc92943aee45
Azha Balouch <azha.nawaz@hotmail.com>
2025-04-28 12:43:47 -0800

    Redifined Structure.

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Welcome from "./user → authentication/welcome";
+
+export default function Home() {
+  return (
+    <>
+      <Welcome />
+    </>
+  );
+}
---
+++ frontend/src/app/user → authentication/welcome.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Welcome(){
+  return (
+    <>
+      <h1 className='page-title'>Welcome Page</h1>
+      <p>We will be working.</p>
+    </>
+  );
+}
