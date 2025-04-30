db767e20d4c63938e93864e9a1fa4d9b47f94427
Azha Balouch <azha.nawaz@hotmail.com>
2025-04-30 09:51:40 -0800

    forgot to use prettier and some error correction

--- frontend_react/src/app/page.tsx
+++ frontend_react/src/app/page.tsx
@@ -2,13 +2,16 @@ export default function Home() {
   return (
     <>
       <h1>Hello, Next.js!</h1>
-      <h1 className="text-3xl font-bold underline font-poppins">
+      <h1 className="font-poppins text-3xl font-bold underline">
         Hello world! Home
       </h1>
-      <section className="bg-background text-text-dark py-20 px-6">
-        <h1 className="text-hero text-primary-dark font-extrabold">Design System Hero</h1>
-        <p className="text-body text-secondary mt-4 max-w-xl font-body">
-          Build consistent interfaces faster with design tokens powered by Tailwind.
+      <section className="bg-background text-text-dark px-6 py-20">
+        <h1 className="text-hero text-primary-dark font-extrabold">
+          Design System Hero
+        </h1>
+        <p className="text-body text-secondary font-body mt-4 max-w-xl">
+          Build consistent interfaces faster with design tokens powered by
+          Tailwind.
         </p>
       </section>
     </>
