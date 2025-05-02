31cd9cbd45929c573e688dbaad39a87e5090c7a3
azhabalouch <azha.nawaz@hotmail.com>
2025-05-03 00:30:19 +0500

    Prettier used

--- frontend_react/src/app/page.tsx
+++ frontend_react/src/app/page.tsx
@@ -9,40 +9,42 @@ export default function Home() {
   const buttonGray = "bg-gray-200 w-[50%] p-5 text-gray-500";
   return (
     <>
-      <h1 className="text-page text-background font-extrabold mt-5 mb-5 ml-48 self-start">UOL Project Collab</h1>
-    <div className="bg-background flex w-[80%] flex-row items-start rounded-2xl p-[2rem]">
-      <section className="w-[60%]">
-        <img src="/college-students.png" />
-        <h1 className="text-hero text-text-dark font-extrabold">Hi there.</h1>
-        <p className="text-sub text-text-dark font-bold">
-          Want your result before your degree?
-          <br />
-          You are at right place.
-        </p>
-      </section>
-      <section className="ml-4 h-full w-[40%]">
-        <div className="flex items-center justify-evenly">
-          <button
-            className={isLoginTab ? "w-[50%] p-5" : buttonGray}
-            onClick={() => {
-              SetLoginTab(true);
-            }}
-          >
-            Login
-          </button>
-          <button
-            className={isLoginTab ? buttonGray : "w-[50%] p-5"}
-            onClick={() => {
-              SetLoginTab(false);
-            }}
-          >
-            Register
-          </button>
-        </div>
+      <h1 className="text-page text-background mt-5 mb-5 ml-48 self-start font-extrabold">
+        UOL Project Collab
+      </h1>
+      <div className="bg-background flex w-[80%] flex-row items-start rounded-2xl p-[2rem]">
+        <section className="w-[60%]">
+          <img src="/college-students.png" />
+          <h1 className="text-hero text-text-dark font-extrabold">Hi there.</h1>
+          <p className="text-sub text-text-dark font-bold">
+            Want your result before your degree?
+            <br />
+            You are at right place.
+          </p>
+        </section>
+        <section className="ml-4 h-full w-[40%]">
+          <div className="flex items-center justify-evenly">
+            <button
+              className={isLoginTab ? "w-[50%] p-5" : buttonGray}
+              onClick={() => {
+                SetLoginTab(true);
+              }}
+            >
+              Login
+            </button>
+            <button
+              className={isLoginTab ? buttonGray : "w-[50%] p-5"}
+              onClick={() => {
+                SetLoginTab(false);
+              }}
+            >
+              Register
+            </button>
+          </div>
 
-        {isLoginTab ? <LoginForm /> : <RegisterForm />}
-      </section>
-    </div>
+          {isLoginTab ? <LoginForm /> : <RegisterForm />}
+        </section>
+      </div>
     </>
   );
 }
